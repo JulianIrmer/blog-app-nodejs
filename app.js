50ae39ac99c59617e6298740e10b556ca144ff8d
Julian Irmer <31626388+JulianIrmer@users.noreply.github.com>
2018-09-23 17:57:25 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,81 @@
+const express = require('express');
+const cors = require('cors');
+const mongojs = require('mongojs');
+const port = 5000 || process.env.PORT;
+const DB_URL = 'mongodb://Holly:ikou05667@ds123614.mlab.com:23614/hollydb'
+let seqID = 1;
+
+//connect to mongodb
+const db = mongojs(DB_URL, ['posts']);
+
+db.on('connect', (err) => {
+  if(err){
+    console.log('database error', err)
+  }
+  else{
+    console.log('database connected')
+  }
+});
+
+//setting up the express server
+const server = express();
+
+//middleware
+server.use(express.json());
+server.use(cors());
+
+//load html/css/js from public folder
+server.use(express.static("public"));
+
+//get all posts
+server.get('/api', (req, res) => {
+  db.posts.find((err, posts) => {
+    if(err){
+      console.log('database error', err);
+    }
+    else{
+      console.log(posts);
+      if(posts.length < 1){
+        res.send('No data');
+      }
+      else{
+        res.json(posts);
+      }
+    }
+  });
+});
+
+//save a post
+server.post('/api/send', (req, res) => {
+  let data = req.body;
+  data.id = seqID;
+  db.posts.save(data);
+  seqID++;
+});
+
+//delete all posts
+server.post('/api/delete', (req, res) => {
+  db.posts.remove({});
+  seqID = 1;
+  console.log('removed all posts');
+});
+
+//delete single post
+server.post('/api/delete/:id', (req, res) => {
+  let postid = JSON.parse(req.params.id);
+  
+  db.posts.remove({id: postid}, (err, post) => {
+    if(err){
+      console.log('database error', err);
+    }
+    else{
+      console.log('Delete Post with ID '+postid);
+    }
+  });
+});
+
+//starting the server
+server.listen(port, () => {
+  console.log('Server listening on port '+port);
+});
+
